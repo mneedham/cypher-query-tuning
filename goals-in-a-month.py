244afd305810e9ed7cc3716aa0103d0d463088ea
Mark Needham <mark.needham@neotechnology.com>
2013-11-06 14:35:12 +0000

    one more go

--- goals-in-a-month.py
+++ goals-in-a-month.py
@@ -25,13 +25,21 @@ attempts = [
 			 ORDER BY month.position
 			 RETURN month.name, REDUCE(total = 0, g in allTheGames | total + g.goals) AS totalGoals'''},
 
-{"query": '''MATCH (player:Player)-[:scored_in]->game
-			 WHERE player.name = "Michu"
-			 WITH DISTINCT player, game
-			 MATCH game<-[:in_month]-month, player-[:played]->stats-[:in]->game
+{"query": '''MATCH (player:Player)-[:played]->stats-[:in]->game
+			 WHERE player.name = "Michu" AND stats.goals > 0
+			 WITH player, game, stats
+			 MATCH game<-[:in_month]-month
           	 WITH month, SUM(stats.goals) AS totalGoals
 			 ORDER BY month.position
-			 RETURN month.name, totalGoals'''},			 
+			 RETURN month.name, totalGoals'''},	
+
+{"query": '''MATCH (player:Player)-[:played]->stats-[:in]->game
+			 WHERE player.name = "Michu" AND stats.goals > 0
+			 WITH player, game, stats.goals AS goals
+			 MATCH game<-[:in_month]-month
+          	 WITH month, SUM(goals) AS totalGoals
+			 ORDER BY month.position
+			 RETURN month.name, totalGoals'''},	
 ]
 
 qp.profile(attempts, iterations=100, runs=3)
