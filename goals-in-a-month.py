b7f1d78d73575a1f3ff0af46a51aad50e1fe2eb7
Mark Needham <mark.needham@neotechnology.com>
2013-11-06 14:26:58 +0000

    trying new style

--- goals-in-a-month.py
+++ goals-in-a-month.py
@@ -24,6 +24,14 @@ attempts = [
 			 WITH month, COLLECT(DISTINCT stats) AS allTheGames
 			 ORDER BY month.position
 			 RETURN month.name, REDUCE(total = 0, g in allTheGames | total + g.goals) AS totalGoals'''},
+
+{"query": '''MATCH (player:Player)-[:scored_in]->game
+			 WHERE player.name = "Michu"
+			 WITH DISTINCT player, game
+			 MATCH game<-[:in_month]-month, player-[:played]->stats-[:in]->game
+          	 WITH month, SUM(stats.goals) AS totalGoals
+			 ORDER BY month.position
+			 RETURN month.name, totalGoals'''},			 
 ]
 
 qp.profile(attempts, iterations=100, runs=3)
