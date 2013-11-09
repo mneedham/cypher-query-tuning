a754007a13f568b420e8c5110d4c627d263b8801
Mark Needham <mark.needham@neotechnology.com>
2013-11-09 12:08:21 +0000

    deleted logging by mistake

--- query_profiler.py
+++ query_profiler.py
@@ -1,4 +1,6 @@
 import timeit
+import re
+import numpy as np
 
 def profile(attempts, iterations=10, runs=3):	
 	for attempt in attempts:
@@ -9,3 +11,7 @@ from query_profiler import a
 import neo4j
 '''
 		timings = timeit.repeat("neo4j.Neo4j().query(a)", setup=setup, number=iterations, repeat=runs)
+		print ""		
+		print re.sub('\n[ \t]', '\n', re.sub('[ \t]+', ' ', attempt['query']))
+		print "Min", np.min(timings), "Mean", np.mean(timings), "95%", np.percentile(timings, 95), "Max", np.max(timings)
+		print ""
